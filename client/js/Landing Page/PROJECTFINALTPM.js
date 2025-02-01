6156370129025b315ace4b43d61d2dab55d200ed
Maxwell Nathaniel Pramita <maxwellnathaniel385@gmail.com>
2025-02-01 20:47:12 +0700

    landingPage\

---
+++ client/js/Landing Page/PROJECTFINALTPM.js
@@ -0,0 +1,48 @@
+document.getElementById("landpage-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("about-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("hadiah-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("juri-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("mentor-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("timeline-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("faq-link").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
